5d4ae78dfc85987dd58dff295a9149aadec5e0ad
yonaries <yonatandejene001@gmail.com>
2022-10-08 22:33:11 +0300

    yarn buld fixed

---
+++ components/Experience.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Experience = () => {
+    return (
+        <div className='grid grid-cols-2'>
+            <div className='h-48 bg-dusk'>
+
+            </div>
+        </div>
+    )
+}
+
+export default Experience
